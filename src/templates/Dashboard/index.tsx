ba1b4edafa63b743585b972d08958471e6836f41
Junior Alves <jjalves96@gmail.com>
2021-02-19 15:29:42 -0300

    chore: import new components

--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useRef } from 'react';
-import { Form } from '@unform/web';
+import { FormEvent, useCallback, useRef, useState } from 'react';
 import { FormHandles } from '@unform/core';
 import { BiSearch } from 'react-icons/bi';
 
@@ -8,15 +7,26 @@ import Main from 'components/Main';
 import Input from 'components/Input';
 import Button from 'components/Button';
 import Title from 'components/Title';
+import CardInfo from 'components/CardInfo';
+import ActionBar from 'components/ActionBar';
+import api from 'services/api';
 
 import * as S from './styles';
 
 const Dashboard: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const [searchPeople, setSearchPeople] = useState('');
 
-  const handleSubmit = useCallback(() => {
-    console.log('foi');
-  }, []);
+  const handleSubmit = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault;
+
+      const response = await api.get(`${searchPeople}`);
+
+      console.log(response);
+    },
+    [searchPeople]
+  );
 
   return (
     <S.Container>
@@ -24,16 +34,20 @@ const Dashboard: React.FC = () => {
       <Main>
         <S.Content>
           <Title>Cadastro -{'>'} Pessoas</Title>
-          <Form onSubmit={handleSubmit} ref={formRef}>
+          <S.FormContainer onSubmit={handleSubmit} ref={formRef}>
             <Input
               name="search"
               type="text"
+              value={searchPeople}
+              onChange={(e) => setSearchPeople(e.target.value)}
               placeholder="Pesquisar pelo nome ou CPF..."
             />
-            <Button>
+            <Button type="submit">
               <BiSearch />
             </Button>
-          </Form>
+          </S.FormContainer>
+          <ActionBar />
+          <CardInfo />
         </S.Content>
       </Main>
     </S.Container>
