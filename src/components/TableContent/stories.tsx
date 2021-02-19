7eb42d377ac2043aeb47d8c30dc5c861f3a61066
Junior Alves <jjalves96@gmail.com>
2021-02-19 18:28:12 -0300

    feat: create TableContent component

---
+++ src/components/TableContent/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import TableContent from '.';
+
+export default {
+  title: 'TableContent',
+  component: TableContent
+} as Meta;
+
+export const Default: Story = () => <TableContent />;
