578acdd0e29177712e6fd478dc4610a1ad8fd8ba
Junior Alves <jjalves96@gmail.com>
2021-02-20 06:21:40 -0300

    chore: complete CardInfo component

--- src/components/CardInfo/index.tsx
+++ src/components/CardInfo/index.tsx
@@ -1,10 +1,14 @@
 import * as S from './styles';
 
+// interface CardInfoProps {
+//   name?: string;
+// }
+
 const CardInfo = () => {
   return (
     <S.Container>
       <S.Content>
-        <S.Title>Esporádicos</S.Title>
+        <S.Title aria-label="esporádicos">Esporádicos</S.Title>
         <S.InfoItem>asasasasasasasasasasaasasasasasasasassas</S.InfoItem>
         <S.InfoItem>asasasas</S.InfoItem>
         <S.InfoItem>asasasas</S.InfoItem>
--- src/components/CardInfo/test.tsx
+++ src/components/CardInfo/test.tsx
@@ -1,15 +1,13 @@
-import { render, screen } from '@testing-library/react';
-
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 import CardInfo from '.';
 
-describe('<CardInfo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<CardInfo />);
+describe('CardInfo', () => {
+  it('should render CardInfo component', () => {
+    renderWithTheme(<CardInfo />);
 
-    expect(
-      screen.getByRole('heading', { name: /CardInfo/i })
-    ).toBeInTheDocument();
-
-    expect(container.firstChild).toMatchSnapshot();
+    expect(screen.getByLabelText(/esporádicos/i)).toHaveStyle({
+      color: '#eea258'
+    });
   });
 });
