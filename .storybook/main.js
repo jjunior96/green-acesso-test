e3015d63ac4c140fdec537e2a01ddc521614e600
Junior Alves <jjalves96@gmail.com>
2021-02-20 06:20:03 -0300

    chore: add config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/components/**/stories.tsx"
+//   ],
+//   "addons": [
+//     "@storybook/addon-essentials",
+//   ]
+// }
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/components/**/stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-  ]
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
