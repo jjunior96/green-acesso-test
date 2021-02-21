16949e6771ef411496b710dc447602219907635a
Junior Alves <jjalves96@gmail.com>
2021-02-21 10:28:25 -0300

    chore: complete Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,17 @@ import { useSelector } from 'react-redux';
 
 import Title from 'components/Title';
 import * as S from './styles';
+import { IState } from 'store';
 
 const Header: React.FC = ({ children }) => {
-  const state = useSelector((state) => state);
-
-  // console.log(state);
+  // const { name } = useSelector<IState>((state) => state.user);
 
   return (
     <S.Container aria-label="header">
       <Title>{children}</Title>
       <S.Content>
         <S.Info>
-          Seja Bem Vindo! <span>Soon Sam</span>{' '}
+          Seja Bem Vindo! <span>Soon Sam</span>
         </S.Info>
       </S.Content>
     </S.Container>
