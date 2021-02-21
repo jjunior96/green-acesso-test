5dec5dd8453b0f167b963218d8344755c63f5b57
Junior Alves <jjalves96@gmail.com>
2021-02-21 07:12:33 -0300

    chore: clean up

--- src/components/CardInfo/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0';
-import CardInfo from '.';
-
-export default {
-  title: 'CardInfo',
-  component: CardInfo
-} as Meta;
-
-export const Default: Story = () => <CardInfo color="otherTitle" />;
