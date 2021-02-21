34d81f0cc6a7542a7e7b8dc95df149a384abbfe3
Junior Alves <jjalves96@gmail.com>
2021-02-21 10:30:42 -0300

    chore: add user reducer

--- src/store/modules/user/reducer.ts
+++ src/store/modules/user/reducer.ts
@@ -2,11 +2,23 @@ import { Reducer } from 'redux';
 import { IUserState } from './types';
 
 const INITIAL_STATE: IUserState = {
-  name: ''
+  name: 'Soon Sam'
 };
 
-const user: Reducer<IUserState> = () => {
-  return INITIAL_STATE;
+const user: Reducer<IUserState> = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'ADD_NAME_TO_USER': {
+      const { name } = action.payload;
+
+      return {
+        name: name
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 };
 
 export default user;
